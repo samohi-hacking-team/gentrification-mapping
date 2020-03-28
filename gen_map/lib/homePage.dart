10db88893904a509fe20b8524817b044078ddabe
jasonTelanoff <jason.telanoff@gmail.com>
2020-03-27 23:58:56 -0700

    Started adding a map and keys

--- gen_map/lib/homePage.dart
+++ gen_map/lib/homePage.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return null;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Gen Map'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
   }
 }
