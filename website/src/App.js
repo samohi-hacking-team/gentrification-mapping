b61ce9a306f3fba99c557e44069784d187f0b4d6
Isabelle Wagenvoord <isabellewagenvoord@gmail.com>
2020-03-28 16:29:39 -0700

    add react stuff 🔥

---
+++ website/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Map = () => <svg id="root"></svg>
+
+function App() {
+  return (
+    <div className="App">
+      <Map/>
+    </div>
+  );
+}
+
+export default App;
