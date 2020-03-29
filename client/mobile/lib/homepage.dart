7ab0d11967ae3c662f9d3288cbee4faa8bf39857
Benjamin Swerdlow <swerdlowbenjamin@gmail.com>
2020-03-28 17:14:02 -0700

    Started homepage

--- client/mobile/lib/homepage.dart
+++ client/mobile/lib/homepage.dart
@@ -13,20 +13,75 @@ class HomePage extends StatelessWidget {
           sliver: SliverStaggeredGrid.count(
             crossAxisCount: 2,
             staggeredTiles: [
-              StaggeredTile.count(1, 2),
-              StaggeredTile.count(1, 1),
-              StaggeredTile.count(1, 1),
-              StaggeredTile.count(2, 1),
+              StaggeredTile.count(1, 1.5),
+              StaggeredTile.count(1, .75),
+              StaggeredTile.count(1, .75),
+              StaggeredTile.count(2, .5),
               StaggeredTile.count(2, .5),
             ],
             children: [
               FlatButton(
+                padding: EdgeInsets.all(15),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(25)),
-                color: Color(0xff3b8bc4),
+                color: Color(0xff5FC9F8),
                 onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      "View Nationwide Map",
+                      textScaleFactor: 1.5,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              FlatButton(
+                padding: EdgeInsets.all(15),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25)),
+                color: Color(0xffFECB2E),
+                onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      "Gentrification information",
+                      textScaleFactor: 1.5,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              FlatButton(
+                padding: EdgeInsets.all(15),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25)),
+                color: Color(0xffFC3158),
+                onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      "Sources",
+                      textScaleFactor: 1.5,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+
+              FlatButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25)),
+                color: Color(0xff53D769),
+                onPressed: ()=>Share,
                 child: Text(
-                  "SMH",
+                  "Share",
                   textScaleFactor: 1.5,
                   style: TextStyle(
                       fontWeight: FontWeight.w800, color: Colors.white),
@@ -35,46 +90,14 @@ class HomePage extends StatelessWidget {
               FlatButton(
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(25)),
-                color: Color(0xff3b8bc4),
-                onPressed: () {},
-                child: Text(
-                  "SMH",
-                  textScaleFactor: 1.5,
-                  style: TextStyle(
-                      fontWeight: FontWeight.w800, color: Colors.white),
+                color: Color(0xff147EFB),
+                onPressed: () => showAboutDialog(
+                  context: context,
+                  applicationName: "Gentrification in the USA",
+                  applicationLegalese: "Isabelle Wagenvoord, Benjamin Swerdlow, Jessica Golden, Jason Telanoff, and Alana Ginter designed this app to help make information about gentrification and displacement more accessible so we can start finding solutions for it."
                 ),
-              ),
-              FlatButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25)),
-                color: Color(0xff3b8bc4),
-                onPressed: () {},
                 child: Text(
-                  "SMH",
-                  textScaleFactor: 1.5,
-                  style: TextStyle(
-                      fontWeight: FontWeight.w800, color: Colors.white),
-                ),
-              ),
-              FlatButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25)),
-                color: Color(0xff3b8bc4),
-                onPressed: () {},
-                child: Text(
-                  "SMH",
-                  textScaleFactor: 1.5,
-                  style: TextStyle(
-                      fontWeight: FontWeight.w800, color: Colors.white),
-                ),
-              ),
-              FlatButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25)),
-                color: Color(0xff3b8bc4),
-                onPressed: () {},
-                child: Text(
-                  "SMH",
+                  "About the app",
                   textScaleFactor: 1.5,
                   style: TextStyle(
                       fontWeight: FontWeight.w800, color: Colors.white),
@@ -118,10 +141,10 @@ class HomePage extends StatelessWidget {
               //   centerItems: true,
               // ),
             ],
-            mainAxisSpacing: 15.0,
-            crossAxisSpacing: 15.0,
+            mainAxisSpacing: 25.0,
+            crossAxisSpacing: 25.0,
           ),
-          padding: EdgeInsets.all(10),
+          padding: EdgeInsets.all(15),
         )
       ]),
     );
